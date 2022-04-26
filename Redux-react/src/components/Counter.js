f6eb334daeba47f03c4aaf57d550e3a61bcee879
Zaman <Zamanmohamed97@gmail.com>
2022-04-27 04:44:09 +0530

    upto-Payloads

---
+++ Redux-react/src/components/Counter.js
@@ -0,0 +1,35 @@
+import { useSelector, useDispatch } from "react-redux";
+import classes from "./Counter.module.css";
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
+
+  const incrseHandler = () => {
+    dispatch({ type: "increase", amount: 5 });
+  };
+
+  const toggleCounterHandler = () => {};
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>increment</button>
+        <button onClick={incrseHandler}>incrase by 5</button>
+        <button onClick={decrementHandler}>decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
